30e6cd6aca105c68af233fa899dbfb845741cb06
CalvinCrawford <calvincrawford831@yahoo.com>
2017-09-08 08:15:18 -0400

    alasql attempt
    same

--- PTA/www/js/Second.js
+++ PTA/www/js/Second.js
@@ -1,3 +1,31 @@
+var alasql = require('alasql');
+
+var db = new alasql.Database();
+
 
 var tom = document.getElementById("bob").innerHTML;
 alert(tom);
+
+
+
+
+
+
+
+
+
+
+
+
+function seedNumber() // seeds number
+{
+    "use strict";
+    var seededNum = 4; //seeds number to return
+    return seededNum;
+}
+
+function addQuestion(qType, qNumber) // adds a question to the listing based on seeded number 
+{
+    "use strict";
+    
+}

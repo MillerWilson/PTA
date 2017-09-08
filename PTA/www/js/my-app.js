2a8a94f46cae18d5d416111eefda7f676ad240a3
CalvinCrawford <calvincrawford831@yahoo.com>
2017-09-08 12:19:52 -0400

    nonxdk

--- PTA/www/js/my-app.js
+++ PTA/www/js/my-app.js
@@ -1,7 +1,6 @@
 // Initialize app
 var myApp = new Framework7();
 
-
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
@@ -11,12 +10,43 @@ var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
+var db = {};
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
-});
+   
+    console.log("Device is still ready");
+     var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+    db.transaction(populateDB, errorCB, successCB);
+    console.log("sql has happened");
 
+});
+ 
+
+//alert("ASFA");
+
+// Populate the database 
+function populateDB(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS DEMO');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+// Transaction error callback
+function errorCB(err) {
+    alert("Error processing SQL: " + err);
+}
+
+// Transaction success callback
+function successCB() {
+    alert("success!");
+}
+
+function createdb()
+{
+    "use strict";
+}
 
 function seedNumber() // seeds number
 {
@@ -24,9 +54,8 @@ function seedNumber() // seeds number
     var seededNum = 4; //seeds number to return
     return seededNum;
 }
-alert("ASFA");
+
 function addQuestion(qType, qNumber) // adds a question to the listing based on seeded number 
 {
     "use strict";
-    
 }

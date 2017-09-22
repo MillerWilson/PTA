ac1bc517f95a0301d1fde2bbc15d3373f3103d32
AAAron1 <31667633+AAAron1@users.noreply.github.com>
2017-09-22 10:00:46 -0400

    sync thing

--- PTA/platforms/browser/www/js/modeSelectorpage.js
+++ PTA/platforms/browser/www/js/modeSelectorpage.js
@@ -41,7 +41,7 @@ var modePage =   '<div class="navbar"><div class="navbar-inner"><div class="left
                                             '<div class="item-content">'+
                                                 '<div class="item-inner">'+
                                                     '<div class="item-input">'+
-                                                        '<input type="text" placeholder="1 - 25" readonly id="picker-device">'+
+                                                        '<input type="text" placeholder="10" readonly id="picker-device">'+
                                                     '</div>'+
                                                ' </div>'+
                                             '</div>'+
@@ -50,8 +50,6 @@ var modePage =   '<div class="navbar"><div class="navbar-inner"><div class="left
                                 '</div>'+                              
                         
                                 '<div>'+
-
-                                    '<a href="testPAge.html" class="button button-round active">Go </a>'+
 
                                 '</div>'+
 
@@ -63,3 +61,127 @@ var modePage =   '<div class="navbar"><div class="navbar-inner"><div class="left
                         '</div>'+
                     '</div>'+
         '</div></div>';    
+
+
+
+
+
+var testPage = '<!-- Status bar overlay for full screen mode (PhoneGap) -->'+
+    '<div class="statusbar-overlay"></div>'+
+
+    '<!-- Panels overlay-->'+
+    '<div class="panel-overlay"></div>'+
+    '<!-- Left panel with reveal effect-->'+
+    '<div class="panel panel-right panel-reveal">'+
+        '<div class="content-block">'+
+            '<p>Settings</p>'+
+            '<p>about</p>'+
+
+        '</div>'+
+    '</div>'+
+
+    '<!-- Views -->'+
+    '<div class="views">'+
+        '<!-- Your main view, should have "view-main" class -->'+
+        '<div class="view view-main">'+
+            '<!-- Top Navbar-->'+
+            '<div class="page toolbar-fixed">'+
+                '<div class="navbar">'+
+                    '<div class="navbar-inner">'+
+                        '<div class="left"></div>'+
+                        '<!-- We need cool sliding animation on title element, so we have additional "sliding" class -->'+
+                        '<div class="center sliding">MillerWilson PTA</div>'+
+                        '<div class="right">'+
+
+                            '<a href="#" class="link icon-only open-panel"><i class="icon icon-bars"></i></a>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+                '<!-- Pages container, because we use fixed-through navbar and toolbar, it has additional appropriate classes-->'+
+                '<div class="pages navbar-through toolbar-through">'+
+                    '<!-- Page, "data-page" contains page name -->'+
+                    '<div data-page="testPage" class="page">'+
+                        '<!-- Scrollable page content -->'+
+                        '<div class="page-content">'+
+                            '<div class="content-block">'+
+
+                                '<!--This block contains the form for the Q'+'s & A'+'s-->'+
+                                '<p><b>Which of the following is NOT a type of motherboard expansion slot?</b> </p><br>'+
+'<div class="content-block-inner">'+
+                                '<form>'+
+                                    '<input type="radio" name="choice" value="A"> ISA<br><br>'+
+                                    '<input type="radio" name="choice" value="B"> PCI<br><br>'+
+                                    '<input type="radio" name="choice" value="C"> AGP<br><br>'+
+                                    '<input type="radio" name="choice" value="D"> ATX'+
+                                '</form><br>'+
+                                '</div>'+
+                           
+
+
+                            '<div class="page-content">'+
+                                '<button class="button">Submit Answer</button>'+
+
+                            '</div>'+
+ '</div>'+
+                        '</div>'+
+                        '<!-- Bottom Toolbar-->'+
+                        '<div class="toolbar toolbar-bottom">'+
+                            '<div class="toolbar-inner">'+
+                                '<a href="#" class="button">Previous</a>'+
+
+                                '<a href="#" class="button">Next</a>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

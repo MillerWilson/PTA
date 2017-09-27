d02d4f38b5b1620f3e6673455f1f634f249ed8eb
AAAron1 <31667633+AAAron1@users.noreply.github.com>
2017-09-27 09:59:44 -0400

    1
    1

--- PTA/platforms/browser/www/js/modeSelectorpage.js
+++ PTA/platforms/browser/www/js/modeSelectorpage.js
@@ -88,7 +88,7 @@ var testPage =
 
     '<div class="page-content">' +
           '<!-- Panels overlay-->' +
-    //'<div class="panel-overlay"></div>' +
+    '<div class="panel-overlay"></div>' +
     '<div class="panel panel-right panel-cover">' +
     '<div id = panel class="content-block">' +
     '<br>' +
